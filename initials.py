f28af99d97c5f730ed27e8bc08cc24e8e5197cd2
63777-hue <2425403861@live.gctu.edu.gh>
2026-05-31 14:32:20 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,6 +1,35 @@
+File: initials.py
+# Description: Print out my initials program.
+# Assignment Number: 2
+#
+# Name: Susana Asabea Yeboah
+# STUDENT ID: 2425403861
+# Email: 2425403861@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Susana, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    """Print stylized large block initials."""
+
+    print()
+    print("...SAY")
+    print()
+
+    print("...SSSSSSSSSSSS......AAAAAAAAAA......Y          Y......")
+    print("...SSSSSSSSSSSS......AA      AA...... Y        Y ......")
+    print("...SS............ ...AA      AA......  Y      Y  ......")
+    print("...SS............ ...AA      AA......   Y    Y   ......")
+    print("...SSSSSSSSSSSS......AAAAAAAAAA......    Y  Y    ......")
+    print("............SS......AA      AA......     YY     ......")
+    print("............SS......AA      AA......     YY     ......")
+    print("............SS......AA      AA......     YY     ......")
+    print("...SSSSSSSSSSSS......AA      AA......     YY     ..**")
+    print("...SSSSSSSSSSSS......AA      AA......     YY     ..**")
+
+    print()
 
 
 
